43fc8eb3df066cd7c08531d33a213b065271844a
Varun Kumar <varungecsiwan@gmail.com>
2024-12-15 02:19:41 +0530

    add more notes

---
+++ basics1/operations.js
@@ -0,0 +1,45 @@
+// Airthematic operation
+
+// 5+5
+// 10-5
+// 5*5
+// 10/2
+// 2**3 = 2*2*2
+// 10%3 = remainder
+
+let valueOne = 3
+valueOne++
+valueOne = valueOne + 1
+
+const numberOne = "2"
+const numberTwo = "4"
+
+const numberThree = numberOne + numberTwo
+console.log(numberThree);
+
+//assignment operator
+
+let valueTwo = 3
+valueTwo += 10
+
+//comparison operator
+
+let pw = 3
+let pwSkills = "3"
+
+console.log(pw === pwSkills)
+
+let pw1 = 3
+let pwSkills1 = "3"
+
+console.log(pw1 == pwSkills1)
+
+let pw2 = 3
+let pwSkills2 = 3
+
+console.log(pw2 == pwSkills2)
+
+let pw3 = 3
+let pwSkills3 = 4
+
+console.log(pw3 != pwSkills3)
